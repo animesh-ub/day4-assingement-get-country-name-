4f7de68e283d005fd2d8f114c2eb492f7a4f22df
animesh-ub <153833091+animesh-ub@users.noreply.github.com>
2023-12-21 17:39:34 +0530

    Add files via upload

---
+++ Display Country Name/script.js
@@ -0,0 +1,13 @@
+// const Countries =[];
+
+async function fetchCountriesName() {
+  let countriesName = document.getElementById("countryDropDown");
+    const response = await fetch("https://countriesnow.space/api/v0.1/countries");
+    const data = await response.json();
+    console.log(data.data);
+    for (let i = 0; i < data.data.length; i++) {
+        // Countries.push(data.data[i].name);
+        countriesName.innerHTML+= `<option> ${data.data[i].country}  </option>`;
+    }
+}
+fetchCountriesName();
